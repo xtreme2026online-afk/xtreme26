054d515316eee0d669071e57a16eba680ca52304
Juju Anselum J <anselumjuju@gmail.com>
2026-03-08 20:10:24 +0530

    feat: add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles/global.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import './styles/global.css';
+import {Analytics} from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
