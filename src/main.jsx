83cf4b83b4e9c9dbdafc03260e86c906fe2d3871
Juju Anselum J <anselumjuju@gmail.com>
2026-03-14 21:46:46 +0530

    feat: add speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
 import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 );
